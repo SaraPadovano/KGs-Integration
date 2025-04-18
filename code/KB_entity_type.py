beacc874dca7e4a71e2ffd740b725ec6fbe81856
SaraPadovano <padovano.sara23@gmail.com>
2025-04-18 19:35:24 +0200

    aggiunta funzione per definire le stringhe query in base al KG in base anche ad un flag

--- code/KB_entity_type.py
+++ code/KB_entity_type.py
@@ -1,47 +1,48 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
-#get_ipython().system(' pip install SPARQLWrapper')
-#get_ipython().system(' pip install python-dateutil')
-
-
-# In[ ]:
-
-
 from SPARQLWrapper import SPARQLWrapper
-from rdflib import Graph
-import _pickle as cPickle
-import pandas as pd
-import rdflib
 import os
 import re
 os.environ['TF_CPP_MIN_LOG_LEVEL']='4'
 
 
+def query_sparkle(Q, KG1_flag):
 
-# In[ ]:
+    if KG1_flag:
+        queryString = """
+            PREFIX dcterms: <http://purl.org/dc/terms/> 
+            PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+            PREFIX ns1: <https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#>
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+            SELECT DISTINCT ?obj WHERE{
+            """ + Q + """ rdf:type ?obj
+            FILTER strstarts(str(?obj), str(dbo:))
+            }
+            """
+        return queryString
+    else:
+        queryString = """
+                    PREFIX : <http://example.org/abuse-of-women#>
+                    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                    PREFIX owl: <http://www.w3.org/2002/07/owl#> 
+                    SELECT DISTINCT ?obj WHERE{
+                    """ + Q + """ rdf:type ?obj
+                    FILTER strstarts(str(?obj), str(dbo:))
+                    }
+                    """
+        return queryString
 
 
-def getRdfType(Q):
+def getRdfType(Q, KG1_flag):
     Q_types = []
 
-    queryString = """
-    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-    PREFIX dbr: <http://dbpedia.org/resource>
-    PREFIX dbo: <http://dbpedia.org/ontology>
-    SELECT DISTINCT ?obj WHERE{
-    """+ Q +""" rdf:type ?obj
-    FILTER strstarts(str(?obj), str(dbo:))
-    }
-    """
+    query = query_sparkle(Q, KG1_flag)
 
-    sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    if KG1_flag:
+        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+    else:
+        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
-    sparql.setQuery(queryString)  
+    sparql.setQuery(query)
     sparql.setTimeout(1000)
 
     sparql.setReturnFormat("json")
@@ -49,7 +50,10 @@ def getRdfType(Q):
     try:
         results = sparql.query().convert()
         for result in results["results"]["bindings"]:
-            Q_types.append(result["obj"]["value"].replace("http://dbpedia.org/ontology/",""))
+            if KG1_flag:
+                Q_types.append(result["obj"]["value"].replace("http://dbpedia.org/ontology/",""))
+            else:
+                Q_types.append(result["obj"]["value"].replace("http://dbpedia.org/ontology/", ""))
         return Q_types
     except TimeoutError:
         return []
@@ -72,19 +76,14 @@ def dataType(string):
     return odp
 
 
-def getRDFData(o):
+def getRDFData(o, KG1_flag):
     if str(o).startswith('http://dbpedia.org/resource/'):
         Q_entity = "<"+o+">"
-        data_type = getRdfType(Q_entity)
+        data_type = getRdfType(Q_entity, KG1_flag)
     else:
         data_type = [dataType(o)]
     
     return o, data_type
-
-
-
-
-# In[ ]:
 
 
 def add_to_set(types, typeset):
