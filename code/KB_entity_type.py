45f21b9646033128dc4f2dd6bbb08c6f120c5a8d
SaraPadovano <padovano.sara23@gmail.com>
2025-04-21 20:27:00 +0200

    modificato per lavorare in locale senza endpoint (stesso problema ma minore velocità con l'endpoint)

--- code/KB_entity_type.py
+++ code/KB_entity_type.py
@@ -1,28 +1,26 @@
 from SPARQLWrapper import SPARQLWrapper
 import os
 import re
+from rdflib import URIRef, RDF
 os.environ['TF_CPP_MIN_LOG_LEVEL']='4'
 
 
 def query_sparql(Q, KG1_flag):
-    print("Entrato in query_sparql")
 
     if KG1_flag:
         print("entrato per query KG1")
         queryString = """
-            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-            PREFIX dcterms: <http://purl.org/dc/terms/> 
-            PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
-            PREFIX ns1: <https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#>
-            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
-            SELECT DISTINCT ?obj WHERE{
-            """ + Q + """ rdf:type ?obj
-            FILTER strstarts(str(?obj), str(ns1:))
-            }
-            """
+                   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                   PREFIX dcterms: <http://purl.org/dc/terms/> 
+                   PREFIX foaf: <http://xmlns.com/foaf/0.1/> 
+                   PREFIX ns1: <https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#>
+                   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+                   SELECT DISTINCT ?obj WHERE{
+                    """ + Q + """ rdf:type ?obj
+                    }
+                   """
         return queryString
     else:
-        print("Entrato per query KG2")
         queryString = """
                     PREFIX : <http://example.org/abuse-of-women#>
                     PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
@@ -36,36 +34,52 @@ def query_sparql(Q, KG1_flag):
         return queryString
 
 
-def getRdfType(Q, KG1_flag):
-    print("Entrato in getRdfType")
+def getRdfType(Q, KG1_flag, graph):
     Q_types = []
 
-    query = query_sparql(Q, KG1_flag)
-    print("Uscito query_sparql")
-    if KG1_flag:
-        print("primo kg endpoint")
-        sparql = SPARQLWrapper("http://localhost:5000/sparql")
-    else:
-        print("secondo kg endpoint")
-        sparql = SPARQLWrapper("http://dbpedia.org/sparql")
-
-    sparql.setQuery(query)
-    sparql.setTimeout(1000)
-
-    sparql.setReturnFormat("json")
-
-    try:
-        results = sparql.query().convert()
-        for result in results["results"]["bindings"]:
-            if KG1_flag:
-                print("kg1 type append")
-                Q_types.append(result["obj"]["value"].replace("https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#", ""))
-            else:
-                print("kg2 type append")
-                Q_types.append(result["obj"]["value"].replace("http://dbpedia.org/ontology/", ""))
-        return Q_types
-    except TimeoutError:
+    # Verifica se è stato passato un grafo
+    if graph is None:
+        print("Errore: nessun grafo passato")
         return []
+
+    query = query_sparql(Q, KG1_flag)
+    print("🧪 SPARQL query generata:\n", query)
+
+    if KG1_flag:
+        try:
+            results = graph.query(query)
+            print(f"✅ Query eseguita, risultati trovati: {len(results)}")
+            for row in results:
+                for val in row:
+                    value = str(val)
+                    Q_types.append(value.replace("https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#", ""))
+            return Q_types
+
+        except Exception as e:
+            print(f"Errore interrogando KG1 locale: {e}")
+            return []
+
+    else:
+        try:
+            sparql = SPARQLWrapper("http://dbpedia.org/sparql")
+            sparql.setQuery(query)
+            sparql.setTimeout(1000)
+            sparql.setReturnFormat("json")
+
+            results = sparql.query().convert()
+            for result in results["results"]["bindings"]:
+                value = result["obj"]["value"]
+                # Aggiungi solo le entità che sono URI validi
+                if value.startswith("http://dbpedia.org/ontology/"):
+                    Q_types.append(value.split("/")[-1])  # Solo l'ID dell'entità
+            return Q_types
+
+        except TimeoutError:
+            print("Timeout DBpedia")
+            return []
+        except Exception as e:
+            print(f"Errore interrogando DBpedia: {e}")
+            return []
 
 def dataType(string):
     odp='string'
@@ -85,30 +99,30 @@ def dataType(string):
     return odp
 
 
-def getRDFData(o, KG1_flag):
-    print("Entrato in getRDFdata")
-    if KG1_flag:
-        if str(o).startswith('https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#'):
-            print("Entrato in if per il primo KG")
-            Q_entity = "<"+o+">"
-            data_type = getRdfType(Q_entity, KG1_flag)
-            print("uscito getRdfType")
-        else:
-            data_type = [dataType(o)]
-        return o, data_type
+def getRDFData(o, KG1_flag, graph):
+    data_type = []
+
+    # Se l'oggetto è un URI, otteniamo i tipi dall'entità
+    if str(o).startswith('https://github.com/PeppeRubini/EVA-KG/tree/main/ontology/ontology.owl#'):
+        # Si tratta di un'entità nel primo Knowledge Graph (KG1)
+        Q_entity = "<" + o + ">"
+        data_type = getRdfType(Q_entity, KG1_flag, graph)  # Ottieni i tipi associati all'entità
+
+    elif str(o).startswith('http://dbpedia.org/resource/'):
+        # Si tratta di un'entità nel secondo Knowledge Graph (KG2)
+        Q_entity = "<" + o + ">"
+        data_type = getRdfType(Q_entity, KG1_flag, graph)
+
     else:
-        if str(o).startswith('http://dbpedia.org/resource/'):
-            print("Entrato in if per il secondo KG")
-            Q_entity = "<"+o+">"
-            data_type = getRdfType(Q_entity, KG1_flag)
-            print("uscito getRdfType")
-        else:
-            data_type = [dataType(o)]
-        return o, data_type
+        # Se l'oggetto non è un URI, si tratta di un valore letterale
+        # Determina il tipo del valore letterale (string, integer, date, etc.)
+        data_type = [dataType(o)]  # Determina il tipo del valore letterale
+        print(f"Tipo letterale identificato: {data_type}")
+
+    return o, data_type
 
 
 def add_to_set(types, typeset):
-    print("entrato in add_to_set")
     for t in types:
         typeset.add(t)
 
